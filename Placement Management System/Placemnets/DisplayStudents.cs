a019b252ba66948c4db483ca75981f41313f91e6
BhaskarBVV <bhaskar.nv3@gmail.com>
2022-09-05 12:06:47 +0530

    Added order by clause in some sql commands

--- Placement Management System/Placemnets/DisplayStudents.cs
+++ Placement Management System/Placemnets/DisplayStudents.cs
@@ -26,7 +26,7 @@ namespace Placement_Management_System.Placemnets
 
         public static void DisplayPlaced()
         {
-            string sqlCommand = "select s.roll_number, name, package, company_name from Student s right join Placed p on s.roll_number = p.roll_number left join Companies c on c.company_id=p.company_id;";
+            string sqlCommand = "select s.roll_number, name, package, company_name from Student s right join Placed p on s.roll_number = p.roll_number left join Companies c on c.company_id=p.company_id order by p.roll_number;";
             MySqlDataReader reader = EditAndSaveDatabase.ReadAndUpdateDatabase(sqlCommand);
 
             if (!reader.HasRows)
@@ -52,7 +52,7 @@ namespace Placement_Management_System.Placemnets
 
         public static void DisplayUnPlaced()
         {
-            string sqlCommand = "select s.roll_number, name from Student s left join Placed p on s.roll_number = p.roll_number  where company_id is NULL;";
+            string sqlCommand = "select s.roll_number, name from Student s left join Placed p on s.roll_number = p.roll_number  where company_id is NULL order by p.roll_number;";
             MySqlDataReader reader = EditAndSaveDatabase.ReadAndUpdateDatabase(sqlCommand);
             Table table = new Table("Roll Number", "Name");
             table.Config = TableConfiguration.MySql();
